c4f461f44593d13e56d1e08814cea9b3ad74ae72
Jeremy Kun <j2kun@users.noreply.github.com>
2024-09-02 13:33:50 -0700

    rename permutation_groups to rotation_groups

--- mask_and_rotate.py
+++ mask_and_rotate.py
@@ -8,17 +8,16 @@ def create_mask(indices: set[int], n: int) -> list[int]:
 
 
 def mask_and_rotate(input: Ciphertext, permutation: dict[int, int]) -> Ciphertext:
-    """Compute the naive approach to permutate the data entries in an FHE ciphertext."""
-
-    # maps a rotation amount to the indices that should be rotated by that amount
-    permutation_groups = defaultdict(set)
+    """Naively permutate the data entries in an FHE ciphertext."""
+    # maps a shift to the indices that should be rotated by that amount
+    rotation_groups = defaultdict(set)
     for i, sigma_i in permutation.items():
-        permutation_groups[sigma_i - i].add(i)
+        rotation_groups[sigma_i - i].add(i)
 
-    print(f"Computing the permutation with {len(permutation_groups)} groups")
+    print(f"Computing the permutation with {len(rotation_groups)} groups")
 
     result = Ciphertext([0] * len(input))
-    for shift, indices in permutation_groups.items():
+    for shift, indices in rotation_groups.items():
         mask = create_mask(indices, len(input))
         result += (input * mask).rotate(shift)
 
