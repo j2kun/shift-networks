b8f39d1ca3a39552c6781135320e18a80517bebc
Jeremy Kun <j2kun@users.noreply.github.com>
2025-09-11 21:43:00 -0700

    get mapping version working?

--- vos_vos_erkin.py
+++ vos_vos_erkin.py
@@ -15,6 +15,19 @@ class RotationGroup:
     indices: frozenset[int]
 
 
+@dataclass(frozen=True)
+class SourceShift:
+    source: int
+    shift: int
+
+
+@dataclass(frozen=True)
+class SourceShiftBits:
+    source: int
+    shift: int
+    shift_bits: list[int]
+
+
 def is_power_of_two(n: int) -> bool:
     """Check if n is a power of two."""
     return n & (n - 1) == 0
@@ -23,36 +36,61 @@ def is_power_of_two(n: int) -> bool:
 def vos_vos_erkin(n: int, mapping: Iterable[tuple[int, int]]) -> list[RotationGroup]:
     assert is_power_of_two(n)
 
-    shifts = [(target - source) % n for (source, target) in mapping]
+    sources = {source for (source, _) in mapping}
     format_string = f"{{:0{n.bit_length() - 1}b}}"
 
     # LSB-to-MSB ordering of bits of each shift
-    shift_bits = [
-        [int(b) for b in reversed(format_string.format(shift))] for shift in shifts
-    ]
+    source_shift_bits: list[SourceShiftBits] = []
+    for (source, target) in mapping:
+        shift = (target - source) % n
+        source_shift_bits.append(
+            SourceShiftBits(
+                source=source,
+                shift=shift,
+                shift_bits=[int(b) for b in reversed(format_string.format(shift))],
+            )
+        )
 
     # Here we compute the coresponding table of values after each rotation,
     # akin to the table in Figure 3 of the paper, excluding the first column
     # of values that are about to be rotated by 1.
-    rounds = []
+    rounds: list[dict[SourceShift, int]] = []
+    rounds.append(
+        {
+            SourceShift(source=ssb.source, shift=ssb.shift): ssb.source
+            for ssb in source_shift_bits
+        }
+    )
+    print()
+    print(rounds[-1])
     for i in range(n.bit_length() - 1):
         rotation_amount = 1 << i
-        last_round = rounds[-1] if rounds else {x: x for x in range(n)}
-        rounds.append(
-            {
-                x: (last_round[x] + rotation_amount if bits[i] == 1 else last_round[x])
-                for (x, bits) in zip(range(n), shift_bits)
-            }
-        )
-        # print(rounds[-1])
+        last_round = rounds[-1]
+        current_round = {}
+        for ssb in source_shift_bits:
+            key = SourceShift(source=ssb.source, shift=ssb.shift)
+            next_position = last_round[key]
+            if ssb.shift_bits[i] == 1:
+                next_position = (last_round[key] + rotation_amount) % n
+            current_round[key] = next_position
+        rounds.append(current_round)
+        print(rounds[-1])
+    print()
 
-    # Any two keys with colliding values in a round require an edge in G.
+    # Any two sources with colliding values in a round require an edge in G.
     G = nx.Graph()
-    for round in rounds:
-        for x, y in itertools.combinations(round.keys(), 2):
-            if round[x] == round[y]:
-                G.add_edge(x, y)
+    for round_num, round in enumerate(rounds):
+        for (ss1, ss2) in itertools.combinations(round.keys(), 2):
+            if ss1.source != ss2.source and round[ss1] == round[ss2]:
+                print(
+                    f"Round {round_num}: collision between {ss1} and {ss2} at {round[ss1]}"
+                )
+                G.add_edge(ss1.source, ss2.source)
 
+    # Vertices are added as edges are added, so no vertices implies no edges,
+    # and all sources can be rotated together.
+    if G.number_of_nodes() == 0:
+        return [RotationGroup(indices=frozenset(sources))]
     coloring = nx.coloring.greedy_color(G, strategy="saturation_largest_first")
 
     indices_by_color = [[] for _ in range(1 + max(coloring.values()))]
--- vos_vos_erkin_test.py
+++ vos_vos_erkin_test.py
@@ -23,13 +23,17 @@ def test_fig3():
     }.items()
     actual = vos_vos_erkin(n, permutation)
     assert len(actual) == 3
-    # FIXME: this test is obsolete after a bug fix
     for bad_edge in [
+        (0, 14),
+        (0, 15),
         (1, 2),
         (1, 3),
+        (2, 3),
         (4, 5),
+        (4, 9),
         (8, 9),
         (11, 12),
+        (11, 13),
         (12, 13),
         (14, 15),
     ]:
@@ -60,3 +64,28 @@ def test_mapping():
         (0, 15),
     ]
     actual = vos_vos_erkin(n, permutation)
+    assert len(actual) == 1
+
+
+def test_mapping_2():
+    n = 16
+    permutation = [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (0, 4),
+        (0, 5),
+        (0, 6),
+        (0, 7),
+        (1, 8),
+        (1, 9),
+        (1, 10),
+        (1, 11),
+        (1, 12),
+        (1, 13),
+        (1, 14),
+        (1, 15),
+    ]
+    actual = vos_vos_erkin(n, permutation)
+    assert len(actual) == 1
